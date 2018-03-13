04de0f9a1bd280b7d6708773cc7400fa3e532f25
Joseph Zoland <joeyzoland@gmail.com>
2018-03-13 12:12:49 -0700

    Added ReverseArray Python Review

---
+++ ReverseArray_3-13-18.py
@@ -0,0 +1,14 @@
+#reverseArray takes in an array and returns a string with all of the characters in reverse order, separated by spaces
+
+def reverseArray(arr):
+    result = "";
+    for x in range (len(arr) - 1, -1, -1):
+        result += str(arr[x]) + " "
+
+    #Removes the final space from the string
+    result = result[:-1]
+
+    print (result)
+
+arr = [0, 3, 4]
+reverseArray(arr)
