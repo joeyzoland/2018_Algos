5108554425683d6b6456a4d4f66613038cc78c59
Joseph Zoland <joeyzoland@gmail.com>
2018-03-16 10:31:49 -0700

    Optimized ArrayLeftRotation from exponential complexity to linear complexity

--- ArrayLeftRotation.py
+++ ArrayLeftRotation.py
@@ -1,16 +1,14 @@
 def leftRotationArray(arr, rotations):
-    for h in range (0, rotations):
-        for i in range (0, len(arr) - 1):
-            temp = arr[i]
-            arr[i] = arr[i + 1]
-            arr[i + 1] = temp
-        print (arr)
+    resultArray = []
     result = ""
+    for h in range (0, len(arr)):
+        trueRotations = (rotations + h) % len(arr)
+        resultArray.append(arr[trueRotations])
     for j in range (0, len(arr)):
-        result += str(arr[j])
-        if (j < len(arr) - 1):
+        result += str(resultArray[j])
+        if (j < len(resultArray) - 1):
             result += " "
     return result
 
 sampleArray = [1, 2, 3, 4, 5]
-print(leftRotationArray(sampleArray, 4))
+print(leftRotationArray(sampleArray, 5))
