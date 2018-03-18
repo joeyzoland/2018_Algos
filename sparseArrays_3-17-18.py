13c6871336efe44776c60f92f88bf750ba4b0ec8
Joseph Zoland <joeyzoland@gmail.com>
2018-03-17 19:51:59 -0700

    completed sparseArrays algo from hackerrank

---
+++ sparseArrays_3-17-18.py
@@ -0,0 +1,11 @@
+def sparseArrays(strings, queries):
+    for i in range (0, len(queries)):
+        count = 0
+        for j in range (0, len(strings)):
+            if (queries[i] == strings[j]):
+                count += 1
+        print (count)
+
+sampleStrings = ['aba', 'baba', 'aba', 'xzxb']
+sampleQueries = ['aba', 'xzxb', 'ab']
+sparseArrays(sampleStrings, sampleQueries)
